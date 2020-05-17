153e2ce5de936a3facbc34b0f519fa04d34f90df
Sunil Bista <072bex444@ioe.edu.np>
2020-05-17 13:07:11 +0545

    added a new component for homepage

---
+++ src/Components/Homepage/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./index.scss";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directoryMenu">
+        <div className="menuItem">
+          <div className="content">
+            <h1 className="title"> Hat </h1>
+            <span className="subTitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menuItem">
+          <div className="content">
+            <h1 className="title"> Hat </h1>
+            <span className="subTitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menuItem">
+          <div className="content">
+            <h1 className="title"> Hat </h1>
+            <span className="subTitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menuItem">
+          <div className="content">
+            <h1 className="title"> Hat </h1>
+            <span className="subTitle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menuItem">
+          <div className="content">
+            <h1 className="title"> Hat </h1>
+            <span className="subTitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
