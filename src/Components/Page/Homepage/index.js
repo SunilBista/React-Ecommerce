c1ab871f1341f1eb1f6997b73f98e19e0e631416
Sunil Bista <072bex444@ioe.edu.np>
2020-08-23 14:18:01 +0545

    new homepage component

---
+++ src/Components/Page/Homepage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+import Directory from "../../Directory";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+    <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
